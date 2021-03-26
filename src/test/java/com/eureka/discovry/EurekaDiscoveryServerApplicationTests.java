4592fde6c93ee3f8d10ad37dbb16be6b964d9051
Harshal Deshmukh <harshaldeshmukh7777@gmail.com>
2021-03-26 11:44:01 +0530

    first commit

---
+++ src/test/java/com/eureka/discovry/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.discovry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
